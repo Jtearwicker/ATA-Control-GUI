9c683d9097851dc50170ff37a4a24a16895eb130
Joel Earwicker <Jtearwicker@gmail.com>
2024-05-09 14:14:07 -0700

    restructuring frames

--- ATA_GUI_Nice.py
+++ ATA_GUI_Nice.py
@@ -17,17 +17,38 @@ customtkinter.CTkButton
 # Create the main root
 root = customtkinter.CTk()
 root.title("ATA Control GUI")
-root.geometry("1200x700")
+root.geometry("1200x800")
 root.configure(background='black')
+customtkinter.set_appearance_mode("Dark")
+
+#Configure frames
+
+#Create a frame for the control tabs
+control_frame = Frame(root, height=300, width=200)
+control_frame.grid(row=1, column=0)
+# Create a text widget to display the terminal output
+#control_output = Text(control_frame, wrap=WORD, height=20, width=100)
+control_output.pack(side=LEFT, fill=BOTH, expand=YES)
+# Create a scrollbar for the text widget
+scrollbar = Scrollbar(control_frame, command=control_output.yview)
+scrollbar.pack(side=RIGHT, fill=Y)
+control_output.config(yscrollcommand=scrollbar.set)
+
+tabview = customtkinter.CTkTabview(master=root)
+tabview.grid(row=0,column=0)
+tabview.add("Calibrate")  
+tabview.add("Antenna Setup")
+tabview.add("Observe")  
+tabview.set("Calibrate") 
+
+
 
 # Create a frame for the terminal output
-terminal_frame = Frame(root, height=400, width=800)
-terminal_frame.grid(row=1, column=0)
-
+terminal_frame = Frame(root, height=300, width=200)
+terminal_frame.grid(row=2, column=0)
 # Create a text widget to display the terminal output
 terminal_output = Text(terminal_frame, wrap=WORD, height=20, width=100)
 terminal_output.pack(side=LEFT, fill=BOTH, expand=YES)
-
 # Create a scrollbar for the text widget
 scrollbar = Scrollbar(terminal_frame, command=terminal_output.yview)
 scrollbar.pack(side=RIGHT, fill=Y)
@@ -37,15 +58,7 @@ terminal_output.config(yscrollcommand=scrollbar.set)
 def run_command(command):
   stream = os.popen(command)
   out = stream.read()
-  pyautogui.alert(out)
-
-tabview = customtkinter.CTkTabview(master=root)
-tabview.grid(row=0,column=0)
-
-tabview.add("Calibrate")  
-tabview.add("Antenna Setup")
-tabview.add("Observe")  
-tabview.set("Calibrate")  
+  pyautogui.alert(out) 
 
 
 #calibration tabs
