b262dfd882f9cbd4b7bf0b21adef7bd9ed427cf9
Joel Earwicker <Jtearwicker@gmail.com>
2024-05-13 18:26:52 -0700

    fixing data type

--- ATA_GUI_Nice.py
+++ ATA_GUI_Nice.py
@@ -166,7 +166,7 @@ def list_avail_targets_clicked():
         radec = ga2equ(targets[i])
         elevation = radec2alt(ga2equ(targets[i]))
         if elevation>20:
-            terminal_text.insert(0.0, "Galactic corrdinate "+str(targets[i])+" has an elevation of "+str(elevation)[0:4] +" degrees. RA Dec = "+radec+".\n")
+            terminal_text.insert(0.0, "Galactic corrdinate "+str(targets[i])+" has an elevation of "+str(elevation)[0:4] +" degrees. RA Dec = "+str(radec)+".\n")
             
 
 
