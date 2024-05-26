a984a94758c4cb4e3c46a1d5f7c8539ec2835cec
Joel Earwicker <Jtearwicker@gmail.com>
2024-05-26 15:54:09 -0700

    going back to previous version

--- plot_test.py
+++ plot_test.py
@@ -51,7 +51,7 @@ terminal_text.pack(fill=BOTH, expand=True)
 # Load and resize the image
 image_path = "MWimg.jpg"
 img = Image.open(image_path)
-img=img.resize((560, 560))
+img=img.resize((280, 280))
 img_tk = ImageTk.PhotoImage(img)
 
 # Label to display the image
