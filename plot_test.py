931d70b0b7fe81704f122d32dbd7ef9d936903d9
Joel Earwicker <Jtearwicker@gmail.com>
2024-05-26 12:25:13 -0700

    fixed target calculator

--- plot_test.py
+++ plot_test.py
@@ -49,14 +49,14 @@ terminal_text = customtkinter.CTkTextbox(master=terminal_frame, height=400, widt
 terminal_text.pack(fill=BOTH, expand=True)
 
 # Load and resize the image
-data = image.imread("MWimg.jpg")
-fig, ax = plt.subplots()
-fig.set_size_inches(8, 4)
-ax.imshow(data)
-ax.axis("off")
-canvas = FigureCanvasTkAgg(fig, master=image_frame)
-canvas.draw()
-canvas.get_tk_widget().pack(fill=BOTH, expand=True)
+image_path = "MWimg.jpg"
+img = Image.open(image_path)
+img = img.resize((900, 400), Image.ANTIALIAS)
+img_tk = ImageTk.PhotoImage(img)
+
+# Label to display the image
+image_label = Label(image_frame, image=img_tk)
+image_label.pack(fill=BOTH, expand=True)
 
 def run_test_command(command):
     terminal_text.insert(END, "Testing USRPs...\n")
@@ -106,6 +106,30 @@ def shut_down_antenna_clicked():
     else:
         ac.move_ant_group(antennas, 'atagr', 'none')
         terminal_text.insert(END, "Antenna 1a has been released.\n")
+
+def ga2equ(ga):
+    g1 = radians(ga[0])
+    g2 = radians(ga[1])
+    c1 = radians(192.85948)
+    c2 = radians(27.12825)
+    c3 = radians(32.93192)
+    a = degrees(atan2((cos(g2) * cos(g1 - c3)), (sin(g2) * cos(c2) - cos(g2) * sin(c2) * sin(g1 - c3))) + c1)
+    b = degrees(asin(cos(g2) * cos(c2) * sin(g1 - c3) + sin(g2) * sin(c2)))
+    if a >= 360:
+        a -= 360
+    return [a, b]
+
+def radec2alt(ga):
+    ga = [ga[0], ga[1]]
+    ga = ga2equ(ga)
+    ata = EarthLocation(lat=40.8175 * u.deg, lon=-121.469 * u.deg, height=1007 * u.m)
+    delta = 0 * u.m
+    altaz = AltAz(location=ata, obstime=Time(datetime.datetime.now(), scale='utc'))
+    radec = SkyCoord(ra=ga[0] * u.deg, dec=ga[1] * u.deg)
+    altitude = radec.transform_to(altaz).alt.deg
+    return altitude
+
+targets = [[45,0],[50,0],[55,0],[60,0],[65,0],[70,0],[75,0],[80,0],[85,0],[90,0],[95,0],[100,0],[105,0],[110,0],[115,0],[120,0],[125,0],[130,0],[135,0],[140,0],[145,0],[150,0],[155,0],[160,0],[165,0],[170,0],[175,0],[180,0],[185,0],[190,0],[195,0],[200,0],[205,0],[210,0],[215,0]]
 
 def list_avail_targets_clicked():
     for i in range(0, 35):
